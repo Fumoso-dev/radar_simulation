470b2e28c67ab24cd0859ef8090ba7d4ef004208
Fumoso-dev <78956919+Fumoso-dev@users.noreply.github.com>
2025-07-15 10:38:58 +0200

    Rimuovo file generati e locali, ora ignorati correttamente

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_RadarSim_FILE C:/Users/giuse/Desktop/QtProjects/RadarSim/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/RadarSim.exe)
-set(__QT_DEPLOY_TARGET_RadarSim_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_RadarSim_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
